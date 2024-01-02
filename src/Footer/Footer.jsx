cbe71b1c41db333c96344d71d84756b5c5a1aafc
David Dallas <Daviddallas606@gmail.com>
2024-01-02 10:17:55 +0000

    images added in

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,7 +1,22 @@
+import {Row, Col, Image} from 'react-bootstrap';
+
 const Footer = () => {
     return(
         <>
-        <h1>Footer</h1>
+       
+        <Row>
+            <Col>
+            </Col>
+            <Col>
+            <Image src='GitHub-logo.png' style={{height: '50px', width: '100px'}}fluid/>
+            </Col>
+            <Col>
+            <Image src='Gmail-logo.png' style={{height: '50px', width: '100px'}}fluid/>
+            </Col>
+            <Col>
+            <Image src='LinkedIn-logo.png' style={{height: '50px', width: '100px'}}fluid/>
+            </Col>
+        </Row>
         </>
     )
 }
