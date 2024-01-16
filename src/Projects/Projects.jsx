d26d79c3f534ce372e5757716839e534dd6e7629
David <110460234+DavidCobb606@users.noreply.github.com>
2024-01-16 07:42:48 +0000

    project link added in

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -5,7 +5,11 @@ const Projects = () => {
         <>
         
         <h3>Weather Trend Viewer</h3>
-        <p>This is a full-stack app that allows the user to view weather trends in any parts of the world via customisable parameters. Built using TypeScript with React for the front-end and C# with ASP.NET for the back-end. The source code can be found <a href="https://github.com/DavidJDallas/historical-weather-visualisation" target="_blank">here </a>for the FE and <a href="https://github.com/DavidJDallas/historical-weather-visualisation-BACKEND" target="_blank">here</a> for the BE.</p>
+        <p>This is a full-stack app that allows the user to view weather trends in any parts of the world via customisable parameters. Built using TypeScript with React for the front-end and C# with ASP.NET for the back-end. 
+            </p>
+        <p>
+            The project is a work in progress but is live <a href="weathertrends.netlify.app target=_blank" >here</a> . The source code can be found <a href="https://github.com/DavidJDallas/historical-weather-visualisation" target="_blank">here </a>for the FE and <a href="https://github.com/DavidJDallas/historical-weather-visualisation-BACKEND" target="_blank">here</a> for the BE.
+            </p>
         </>
     )
 }
