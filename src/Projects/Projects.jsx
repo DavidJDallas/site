89d97a5bc338bd82aaf88735a44473cdccec2ad2
David <110460234+DavidCobb606@users.noreply.github.com>
2025-05-06 20:27:34 +0100

    more descriptions added

--- src/Projects/Projects.jsx
+++ src/Projects/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
     return(
         <>
         <h2>Practical based</h2>
-        
+
         <h3>Weather Trend Viewer</h3>
         <p>
         A while back I built an app that allows you to view historical weather trends for any location you'd like to see. It was built as a practical tool for trying to get a rough feel for temperatures and amount of rain you'd likely experience when taking trips to specific places around the world. It utilises the D3 library in JavaScript/TypeScript, and uses React as a front-end library. 
@@ -24,8 +24,6 @@ const Projects = () => {
 
         <h3>Computer Architecture</h3>
         <p>I made some notes to the first few lectures of Geoffrey Messier's online lecture series on Computer Architecture. See here https://github.com/DavidJDallas/Computer-Architecture for my notes, and here for his excellent lecture series. </p>
-
-
 
         </>
     )
