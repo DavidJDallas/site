33582f59cc899e33eca4903c667d44154c498f47
David <110460234+DavidCobb606@users.noreply.github.com>
2025-05-28 17:52:39 +0100

    mobile friendly code added in. email link fixed and footer margins added

--- src/LandingPage/LandingPage.jsx
+++ src/LandingPage/LandingPage.jsx
@@ -16,7 +16,6 @@ const LandingPage = () => {
              <p className='main-body'>
             I love to learn and am driven by a constant desire to understand. Before software engineering, I completed two Masters degrees and a PhD in Philosophy - specifically, in the Philosophy of Science. My Thesis was on Empiricism in Science and the Philosophy of Science, both historically and contemporarily. The phil-sci Archive link can be found <a href="http://philsci-archive.pitt.edu/21458/" target="_blank">here</a>, and the official document can be found <a href="https://research-information.bris.ac.uk/en/studentTheses/empiricism-in-the-philosophy-of-science" target="_blank">here</a>.
 
-            Relevant to 
 
              </p>
 
@@ -26,46 +25,6 @@ const LandingPage = () => {
   
     )
   }
-    
-    return(
-        <>
-        
-          <Row>
-         
-       
-            <Col className='column-body'>  
-            <h2>About Me</h2>
-            <p className='main-body'>I'm a software developer with a background in Academic Research in the Philosophy of Science. I now mainly work in web development, and have commercial experience ranging from SaaS web apps to website design and building to custom-made full-stack web apps. 
-             </p>
-             <p className='main-body'>
-            My Thesis was on Empiricism in Science and the Philosophy of Science. The phil-sci Archive link can be found <a href="http://philsci-archive.pitt.edu/21458/" target="_blank">here</a>, and the official document can be found <a href="https://research-information.bris.ac.uk/en/studentTheses/empiricism-in-the-philosophy-of-science" target="_blank">here</a>.
-
-            Relevant to 
-
-             </p>
-
-             <p className='main-body'>
-              I mainly work in web development, and have experience ranging from SaaS web apps, to websites to custom-made web apps. The languages I'm most comfortable in are JavaScript and TypeScript. I have more experience in the Front-End, where my preferred framework is React; but I nonetheless really enjoy the Back-End too. Within JavaScript and TypeScript, I use Express as a framework. 
-            </p>
-            <p className='main-body'>
-              I'm also familiar with C# and .NET, especially using ASP.NET. Moving forward I'd love to explore more languages. I'm keen to learn a purely functional language (started learning Haskell) and a lower-level language (C/Rust/Zig/Hare) to satisfy my curiosity there. 
-            </p>
-            <p className='main-body'>
-            As you can see, I'm also lucky enough to have had a healthy son born in November 2023. I've been taking some time out of full-time work and doing part-time freelance for a couple of months now. But I'm now open to either permanent, contract, or freelance work. 
-
-            </p>
-             
-
-
-            
-            </Col>
-         
-
-
-
-          </Row>
-        </>
-    )
 }
 
 export default LandingPage
--- src/NavBar/NavBarTop.jsx
+++ src/NavBar/NavBarTop.jsx
@@ -1,11 +1,61 @@
+import './NavBar.css';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Link, NavLink } from 'react-router-dom';
 
+const NavBarTop = ({ setTitle }) => {
+  return (
+    <>
+      <Container fluid className='top-nav-container'>
+        <Row className='justify-content-center'>
+          <Col xs={12}>
+            <div className='top-nav-wrapper'>
+              <div className='top-nav-item'>
+                <NavLink
+                  to='/'
+                  style={({ isActive, isPending }) => ({
+                    color: isActive ? '#fd7f6f' : '#1d2731',
+                    textDecoration: 'none',
+                    transition: 'color 0.3s ease',
+                  })}
+                  onClick={() => setTitle('')}
+                >
+                  <h4>Home</h4>
+                </NavLink>
+              </div>
 
-const NavBarTop = () => {
-    return (
-        <>
-        <h4>Nav Bar Top</h4>
-        </>
-    )
-}
+              <div className='top-nav-item'>
+                <NavLink
+                  to='/projects'
+                  style={({ isActive, isPending }) => ({
+                    color: isActive ? '#fd7f6f' : '#1d2731',
+                    textDecoration: 'none',
+                    transition: 'color 0.3s ease',
+                  })}
+                  onClick={() => setTitle('Projects')}
+                >
+                  <h4>Projects</h4>
+                </NavLink>
+              </div>
 
-export default NavBarTop
+              <div className='top-nav-item'>
+                <NavLink
+                  to='/blog'
+                  style={({ isActive, isPending }) => ({
+                    color: isActive ? '#fd7f6f' : '#1d2731',
+                    textDecoration: 'none',
+                    transition: 'color 0.3s ease',
+                  })}
+                  onClick={() => setTitle('Blog')}
+                >
+                  <h4>Blog</h4>
+                </NavLink>
+              </div>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
+};
+
+export default NavBarTop;
